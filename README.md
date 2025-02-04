22dfaa4507d95b19b2a9b3e115a1afdbdbc97fa4
David Young <david@thedavidyoung.co.uk>
2025-02-04 21:28:48 +0000

    Initial structure and first conversion

---
+++ README.md
@@ -0,0 +1,5 @@
+# openapi31to30
+
+Rust port of Apiture's
+[https://github.com/apiture/openapi-down-convert](openapi-down-convert)
+utility for converting OpenAPI v3.1 schemas to v3.0-compatible formats.
---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use color_eyre::{eyre::eyre, Result};
+use serde::{Deserialize, Serialize};
+use serde_yaml::{Mapping, Value};
+
+const OPENAPI_V_303: &str = "3.0.3";
+
+#[derive(Debug, Deserialize, Serialize)]
+struct OpenApiTopLevel {
+    openapi: String,
+    info: Option<Mapping>,
+    paths: Option<Mapping>,
+    components: Option<Mapping>,
+    tags: Option<Value>,
+}
+
+pub fn convert(schema: &str) -> Result<String> {
+    let mut schema = serde_yaml::from_str::<OpenApiTopLevel>(schema)?;
+
+    match schema.openapi.as_str() {
+        "3.1.0" => {
+            // Expected schema value; this is fine
+        }
+        v30 if v30.starts_with("3.0") => {
+            return Err(eyre!("Schema is already version 3.0"));
+        }
+        other => {
+            return Err(eyre!("Unsupported schema version `{other}`"));
+        }
+    }
+
+    schema.openapi = OPENAPI_V_303.into();
+    remove_licence_identifier(&mut schema);
+    convert_schema_ref(&mut schema);
+
+    Ok(serde_yaml::to_string(&schema)?)
+}
+
+fn remove_licence_identifier(schema: &mut OpenApiTopLevel) -> Option<()> {
+    let licence = schema.info.as_mut()?.get_mut("license")?.as_mapping_mut()?;
+    if let Some(Value::String(info)) = licence.remove("info") {
+        println!("Remove licence info, `{info}`");
+    }
+    None
+}
+
+fn convert_schema_ref(_schema: &mut OpenApiTopLevel) -> Option<()> {
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use insta::assert_snapshot;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn schema_versions() {
+        assert_eq!(
+            convert("").unwrap_err().to_string(),
+            "missing field `openapi`",
+        );
+        assert_eq!(
+            convert("game: {}").unwrap_err().to_string(),
+            "missing field `openapi`",
+        );
+        assert_eq!(
+            convert("openapi: {}").unwrap_err().to_string(),
+            "openapi: invalid type: map, expected a string at line 1 column 10",
+        );
+        assert_eq!(
+            convert("openapi: 3.0.1").unwrap_err().to_string(),
+            "Schema is already version 3.0",
+        );
+        assert_eq!(
+            convert("openapi: 4.0.1").unwrap_err().to_string(),
+            "Unsupported schema version `4.0.1`",
+        );
+    }
+
+    #[test]
+    fn example_from_downconvert() {
+        assert_snapshot!(convert(include_str!("../samples/downconvert.yaml")).unwrap());
+    }
+}
