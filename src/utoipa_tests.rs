9a05a54506277dba7afd81cb268ced6ed7cebbd0
David Young <david@thedavidyoung.co.uk>
2025-02-20 17:47:19 +0000

    Add tests for basic HTTP verbs with utoipa

--- src/utoipa_tests.rs
+++ src/utoipa_tests.rs
@@ -1,26 +1,41 @@
 use crate::{convert, test::progenitor_test};
-use axum::extract::Json;
+use serde::{Deserialize, Serialize};
 use utoipa::openapi::OpenApi;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-#[derive(utoipa::ToSchema, serde::Serialize)]
+mod verbs;
+
+#[derive(utoipa::ToSchema, Deserialize, Serialize)]
 struct User {
     id: i32,
-}
-
-#[utoipa::path(get, path = "/user", responses((status = OK, body = User)))]
-async fn get_user() -> Json<User> {
-    Json(User { id: 1 })
 }
 
 #[test]
 fn basic_get() {
     let (_router, api): (axum::Router, OpenApi) = OpenApiRouter::new()
-        .routes(routes!(get_user))
+        .routes(routes!(verbs::get_user))
         .split_for_parts();
     let yaml = serde_yaml::to_string(&api).unwrap();
     insta::assert_snapshot!("basic-before", &yaml);
     let converted = convert(&yaml).unwrap();
     insta::assert_snapshot!("basic-converted", &converted);
+    progenitor_test(&converted);
+}
+
+#[test]
+fn basic_verbs() {
+    let (_router, api): (axum::Router, OpenApi) = OpenApiRouter::new()
+        .routes(routes!(
+            verbs::get_user,
+            verbs::put_user,
+            verbs::post_user,
+            verbs::patch_user,
+            verbs::delete_user
+        ))
+        .split_for_parts();
+    let yaml = serde_yaml::to_string(&api).unwrap();
+    insta::assert_snapshot!("verbs-before", &yaml);
+    let converted = convert(&yaml).unwrap();
+    insta::assert_snapshot!("verbs-converted", &converted);
     progenitor_test(&converted);
 }
---
+++ src/utoipa_tests/verbs.rs
@@ -0,0 +1,27 @@
+use super::User;
+use axum::extract::Json;
+
+#[utoipa::path(get, path = "/user", responses((status = OK, body = User)))]
+pub async fn get_user() -> Json<User> {
+    Json(User { id: 1 })
+}
+
+#[utoipa::path(put, path = "/user", responses((status = OK, body = User)))]
+pub async fn put_user(Json(user): Json<User>) -> Json<User> {
+    Json(user)
+}
+
+#[utoipa::path(post, path = "/user", responses((status = OK, body = User)))]
+pub async fn post_user(Json(_user): Json<User>) -> Json<User> {
+    Json(User { id: 1 })
+}
+
+#[utoipa::path(patch, path = "/user", responses((status = OK, body = User)))]
+pub async fn patch_user() -> Json<User> {
+    Json(User { id: 1 })
+}
+
+#[utoipa::path(delete, path = "/user", responses((status = OK, body = User)))]
+pub async fn delete_user() -> Json<User> {
+    Json(User { id: 1 })
+}
